47ac96445b15583e9e2c5992b9ff858b36d51d96
Marc Guo <marqueguo@gmail.com>
2018-04-16 20:40:18 -0400

    IShape Visitor revised.

--- CustomerCode/src/cs3500/animator/provider/hw5/shapes/visitor/IShapeVisitor.java
+++ CustomerCode/src/cs3500/animator/provider/hw5/shapes/visitor/IShapeVisitor.java
@@ -1,7 +1,6 @@
 package cs3500.animator.provider.hw5.shapes.visitor;
 
-import cs3500.animator.provider.hw5.shapes.Oval;
-import cs3500.animator.provider.hw5.shapes.Rectangle;
+import cs3500.animator.provider.hw5.shapes.IShape;
 
 /**
  * This interface represent a visitor over all implementations of shapes.
@@ -12,12 +11,12 @@ public interface IShapeVisitor<T> {
    * @param oval a shape that accepts a visitor
    * @return result of applying this visitor to the oval
    */
-  T visitOval(Oval oval);
+  T visitOval(IShape oval);
 
   /**
    * Returns a result of applying this visitor to a rectangle.
    * @param rectangle a shape that accepts a visitor
    * @return result of applying this visitor to the oval
    */
-  T visitRectangle(Rectangle rectangle);
+  T visitRectangle(IShape rectangle);
 }
